f60e2ab50cb4f553970865b53ea5d9f156e7b69a
AL <sycophant29@gmail.com>
2019-08-10 23:03:07 +0100

    ex26.py

--- ex26.py
+++ ex26.py
@@ -1,41 +1,97 @@
-### LPTHW Exercise 26 ###
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+print("How much do you weigh?", end=' '
+weight = input()
 
-#!/usr/bin/python3
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
 
-def break_words(stuff):
-    ### This function will brak up words for us ###
-    words = stuff.split(' ')
-    return words
+script, filename = argv
 
-def sort_words(words):
-    ### Sorts the words ###
-    return sorted(words)
+txt = open(filenme)
 
-def print_first_word(words):
-    ### Prints the first word after popping it off ###
-    word = words.pop(0)
-    print(word)
-    
-def print_last_word(words):
-    ### Prints the last word after popping it off ###
-    word = words.pop(-1)
-    print(word)
-    
-def sort_sentence(sentence):
-    ### Takes in a full sentence and returns the sorted words ###
-    words = break_words(sentence)
-    return sort_words(words)
+print("Here's your file {filename}:")
+print(tx.read())
 
-def print_first_and_last(sentence):
-    ### Prints the first and last words of the sentence ###
-    words = break_words(sentence)
-    print_first_word(words)
-    print_last_word(words)
-    
-def print_first_and_last_sorted(sentence):
-    ### Sorts the words then prints the first and last ones ###
-    words = sort_sentence(sentence)
-    print_first_word(words)
-    print_last_word(words)
-    
-    
+print("Type the filename again:")
+file_again = input("> ")
+
+txt_again = open(file_again)
+
+print(txt_again_read())
+
+
+print('Let's practice everything.')
+print('You\'d need to know \'bout escapes 
+      with \\ that do \n newlines and \t tabs.')
+
+poem = """
+\tThe lovely world
+with logic so firmly planted
+cannot discern \n the needs of love
+nor comprehend passion from intuition
+and requires an explanation
+\n\t\twhere there is none.
+"""
+
+print("--------------)
+print(poem)
+print(--------------")
+
+
+five = 10 - 2 + 3 - 
+print(f"This should be five: {five}"
+
+def secret_formula(started)
+    jelly_beans = started * 500
+    jars = jelly_beans / 1000
+    crates = jars  100
+    return jelly_beans, jars, crates
+
+
+start_point = 10000
+beans, jars = secret_formula(start_point)
+
+# remember that this is another way to format a string
+print("With a starting point of: {}".format(start_point))
+# it's just like with an f"" string
+print(f"We'd have {beans} beans, {jars} jars, and {crates} crates.")
+
+start_point = start_point / 10
+
+print("We can also do that this way:")
+formula = secret_formula(startpoint)
+# this is an easy way to apply a list to a format string
+print("We'd have {} beans, {} jars, and {} crates.".format(*formula))
+
+
+
+people = 20
+cates = 30
+dogs = 15
+
+
+if people < cats:
+    print "Too many cats! The world is doomed!"
+
+if people < cats:
+    print("Not many cats! The world is saved!")
+
+if people < dogs:
+    print("The world is drooled on!")
+
+if people > dogs
+    print("The world is dry!")
+
+
+dogs += 5
+
+if people >= dogs:
+    print("People are greater than or equal to dogs.")
+
+if people <= dogs
+    print("People are less than or equal to dogs.)
+
+
+if people = dogs:
+    print("People are dogs.")
